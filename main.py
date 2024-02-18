7ad3c998b5e431995dca9ec922bf574180f65baa
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2024-02-18 16:23:02 +0000

    Yay!

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ async def ping(interaction: discord.Interaction):
   await interaction.response.send_message(
       "I don't have time to play table tennis!")
 
+
 @bot.event
 async def on_message(message):
   try:
@@ -48,7 +49,8 @@ async def on_message(message):
       return
   if message.author.id not in list(data[str(
       message.guild.id)]["members"].keys()):
-    data[str(message.guild.id)]["members"][str(message.author.id)] = templates["member"]
+    data[str(message.guild.id)]["members"][str(
+        message.author.id)] = templates["member"]
   data[str(message.guild.id)]["members"][str(
       message.author.id)]["messages"] += 1
   data[str(message.guild.id)]["members"][str(
@@ -59,44 +61,55 @@ async def on_message(message):
     json.dump(data, f)
 
 
-@bot.tree.command(name="setup", description="Resets all settings, but can fix errors. ")
+@bot.tree.command(name="setup",
+                  description="Resets all settings, but can fix errors. ")
 @commands.has_permissions(manage_guild=True)
 async def setup(interaction: discord.Interaction):
-  data[str(interaction.guild_id)] = templates[guild]
+  data[str(interaction.guild_id)] = templates["guild"]
   with open("data.json", "w") as f:
     json.dump(data, f)
-  await interaction.response.send_message("All settings have been reset. Now, run ``/settings``")
+  await interaction.response.send_message(
+      "All settings have been reset. Now, run ``/settings``")
 
 
-@bot.tree.command(
-    name="notifychannel",
-    description="""Set the channel to send notifications to on level up""")
-@commands.has_permissions(manage_guild=True)
-async def notifychannel(interaction: discord.Interaction,
-                        channel: discord.TextChannel):
-
- 
-
-@bot.tree.command(name="settings leveling")
+@bot.tree.command(name="leveling_settings")
 @discord.app_commands.describe(
     notify_channel="Set the channel for level up notifications to be sent. ",
-    notify_message="Set the notification message. Use @u to mention the user and @l to mention the level.",
-    xp_by_message="Set the amount of XP a user gets per message. Level up at 100 XP",
-    timeout="Set the amount of time the bot will wait between giving xp from messages. (Seconds)")
+    notify_message=
+    "Set the notification message. Use @u to mention the user and @l to mention the level.",
+    xp_by_message=
+    "Set the amount of XP a user gets per message. Level up at 100 XP",
+    timeout=
+    "Set the amount of time the bot will wait between giving xp from messages. (Seconds)"
+)
 @commands.has_permissions(manage_guild=True)
-async def leveling_settings(interaction: discord.Interaction, 
-    notify_channel:discord.TextChannel=None, 
-    notify_message:str=None, 
-    xp_by_message:int=5,
-    timeout:int=0,
-    ):
+async def leveling_settings(
+    interaction: discord.Interaction,
+    notify_channel: discord.TextChannel = None,
+    notify_message: str = None,
+    xp_by_message: int = 5,
+    timeout: int = 0,
+):
   try:
     data[str(interaction.guild.id)]
   except KeyError:
     await interaction.response.send_message("Please run ``/setup`` first.")
-  args = locals()
-  for option in [for item in args().keys() if args[item] != None and args[item] != interaction]:
-    data[str(interaction.guild_id)]["settings"]["leveling"][option] = args[option]
+    return
+  else:
+    if notify_channel != None:
+      data[str(interaction.guild.id
+               )]["settings"]["notify_channel"] = notify_channel.id
+    if notify_message != None:
+      data[str(
+          interaction.guild.id)]["settings"]["notify_message"] = notify_message
+    if xp_by_message != None:
+      data[str(
+          interaction.guild.id)]["settings"]["xp_by_message"] = xp_by_message
+    if timeout != None:
+      data[str(interaction.guild.id)]["settings"]["timeout"] = timeout
+    with open("data.json", "w") as f:
+      json.dump(data, f)
+    await interaction.response.send_message("Settings have been updated.")
 
 
 bot.run(os.environ['TOKEN'])
